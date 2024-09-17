f862d8d47c17532a36e620ae09e8c14abb9f43da
Daim <daim_choudhary@yahoo.co.uk>
2024-09-17 17:13:18 +0100

    trying to fix slicing issue

--- src/components/FullArticle/FullArticle.tsx
+++ src/components/FullArticle/FullArticle.tsx
@@ -14,40 +14,47 @@ const FullArticle = () => {
         firstPublicationDate,
         thumbnail,
         body,
-    } = fields || {};
+    } = fields || null;
 
     console.log(webTitle);
+    console.log(firstPublicationDate);
 
     return (
-        <div className="fullArticle">
-            <div className="fullArticle__textSection">
-                <h1 className="fullArticle__textSection--heading">
-                    {headline}
-                </h1>
-                <h3 className="fullArticle__textSection--tagline">
-                    {trailText}
-                </h3>
-                <h5 className="fullArticle__textSection--date">
-                    {firstPublicationDate.slice(0, 10)}
-                </h5>
-                <img
-                    src={thumbnail}
-                    alt="Front Image of article"
-                    className="fullArticle__textSection--img"
-                />
-                <h3 className="fullArticle__textSection--author">{byline}</h3>
-                <p
-                    className="fullArticle__textSection--text"
-                    dangerouslySetInnerHTML={{ __html: body }}
-                ></p>
-            </div>
-            <div className="fullArticle__sidebar">
-                <SideBar
-                    url={`https://content.guardianapis.com/${sectionId}?page=2&show-fields=all&api-key=3bdabd26-081c-4be3-acfa-e73952c73518`}
-                    id={id}
-                />
-            </div>
-        </div>
+        <>
+            {fields && (
+                <div className="fullArticle">
+                    <div className="fullArticle__textSection">
+                        <h1 className="fullArticle__textSection--heading">
+                            {headline}
+                        </h1>
+                        <h3 className="fullArticle__textSection--tagline">
+                            {trailText}
+                        </h3>
+                        <h5 className="fullArticle__textSection--date">
+                            {firstPublicationDate.slice(0, 10)}
+                        </h5>
+                        <img
+                            src={thumbnail}
+                            alt="Front Image of article"
+                            className="fullArticle__textSection--img"
+                        />
+                        <h3 className="fullArticle__textSection--author">
+                            {byline}
+                        </h3>
+                        <p
+                            className="fullArticle__textSection--text"
+                            dangerouslySetInnerHTML={{ __html: body }}
+                        ></p>
+                    </div>
+                    <div className="fullArticle__sidebar">
+                        <SideBar
+                            url={`https://content.guardianapis.com/${sectionId}?page=2&show-fields=all&api-key=3bdabd26-081c-4be3-acfa-e73952c73518`}
+                            id={id}
+                        />
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
 export default FullArticle;
